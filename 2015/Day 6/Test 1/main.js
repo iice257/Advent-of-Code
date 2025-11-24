77e6e566fadfc9e764777c088f9f5d99e22577b8
ICE <kingsley.aremu@gmail.com>
2025-11-24 18:50:07 +0100

    Day 6 test 1 continuation

--- 2015/Day 6/Test 1/main.js
+++ 2015/Day 6/Test 1/main.js
@@ -1,14 +1,33 @@
 instructions = document.getElementById('instructions').textContent.split('\n');
-instruction = '';
-for (let i = 0; i < instructions.length; i++) {
-  instruction = instructions[i].split(' ');
-  toggle = instruction[0];
-  from = instruction[1].split(',');
-  to = instruction[3].split(',');
-  light = 0;
-  if (toggle === 'on') {
-    light = 1;
-  }
 
+instruction = [];
+ins = [];
+toggle = ''
+from = ''
+to = ''
+
+for (let o = 0; o < instructions.length; o++) {
+  instruction.push(instructions[o].split(' '));
+  for (let i = 0; i < instruction.length; i++) {
+    ins.push(instruction[i]);
+  }
+  //   if (ins[0] === 'turn') {
+  //     from = ins[2];
+  //     to = ins[4];
+  //   } else {
+  //     toggle = ins[0];
+  //     from = ins[1];
+  //     to = ins[3];
+  //   }
+  //   light = 0;
+  //   if (toggle === 'on') {
+  //     light = 1;
+  //   } else {
+  //     light = 0;
+  //   }
+  // }
 }
-console.log([instructions[1].split(' ')]);
+
+console.log(ins);
+// console.log(ins);
+console.log(from);
