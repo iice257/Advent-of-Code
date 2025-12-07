5bda430832fee283b1f7adf9063e6f128e1e96dc
ICE <103445230+iice257@users.noreply.github.com>
2025-12-07 13:17:05 +0100

    Day 6 tests, completed.
    Well, after a loooong time I finally completed day 6 from the 2016 tests. This was really tricky one for me, and I don't even know why but it took longer than it should mainly because I kept failing to understand the question multiple times but as I grasped it, piece of cake (lol). On to the next

---
+++ 2015/Day 6 - Done/Test 1 - Done/main.js
@@ -0,0 +1,50 @@
+const instructions = document.getElementById('instructions').textContent.split('\n').map((splits) => splits.split(' '));;
+let grid = Array(1000).fill(0).map(() => Array(1000).fill(false));
+
+function getInstuctions(instruction) {
+  let state, x1_string, y1_string, x2_string, y2_string;
+
+  if (instruction[0] === 'toggle') {
+    state = 'toggle';
+    [x1_string, y1_string] = instruction[1].split(',');
+    [x2_string, y2_string] = instruction[3].split(',');
+  } else {
+    state = instruction[1] === 'on' ? 'on' : 'off';
+    [x1_string, y1_string] = instruction[2].split(',');
+    [x2_string, y2_string] = instruction[4].split(',');
+  };
+
+  return { 
+    state: state, 
+    x1: parseInt(x1_string), 
+    y1: parseInt(y1_string), 
+    x2: parseInt(x2_string), 
+    y2: parseInt(y2_string)
+  };
+};
+
+for (let i = 0; i < instructions.length; i++) {
+  const { state, x1, y1, x2, y2 } = getInstuctions(instructions[i]);
+  for (let x = x1; x <= x2; x++) {
+    for (let y = y1; y <= y2; y++) {
+      if (state === 'on') {
+        grid[x][y] = true;
+      } else if (state === 'off') {
+        grid[x][y] = false;
+      } else if (state === 'toggle') {
+        grid[x][y] = !grid[x][y];
+      }
+    };
+  };
+};
+
+let lightsOn = 0;
+for (let x = 0; x < 1000; x++) {
+  for (let y = 0; y < 1000; y++) {
+    if (grid[x][y] === true) {
+      lightsOn++;
+    }
+  }
+};
+
+console.log(lightsOn);
--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 I'm just starting to try out these Advent of Code challenges! They seem like a great way to practice and hone my JavaScript skills, and I'm looking forward to working through them.
 
+The plan is to solve them from the very beginning (2015, day 1) all the way to the latest one (2025 at this time), and continue whenever there's a new one december each year.
+
+The main constraint is that while i'm allowed to google some simple things, there will be no use of AI help whatsoever otherwise this whole thing would be pointless (I even turned off AI on my google searches).
+
 Each day's solution will be in its own folder.
+
+There will obviously be more elegant solutions out there, or shorter, or more efficient, or cleaner, or faster, or ... okay you get the point. But i'm not aiming for any of that, my number 1 goal is to solve the problems, full-stop. Any other objective is secondary.
